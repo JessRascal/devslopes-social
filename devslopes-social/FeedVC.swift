2c6905434226c2d273994470f747ce435040c61c
Jess Rascal <Jess@Jessey.uk>
2016-07-25 00:16:38 +0100

    Completed adding feed tableview foundation code

--- devslopes-social/FeedVC.swift
+++ devslopes-social/FeedVC.swift
@@ -10,15 +10,33 @@ import UIKit
 import Firebase
 import SwiftKeychainWrapper
 
-class FeedVC: UIViewController {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
     
-    @IBAction func singInTapped(_ sender: AnyObject) {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+    }
+    
+    
+    
+    @IBAction func signOutTapped(_ sender: AnyObject) {
         let keychainResult = KeychainWrapper.removeObjectForKey(KEY_UID)
         print("JESS: ID removed from keychain \(keychainResult)")
         try! FIRAuth.auth()?.signOut()
