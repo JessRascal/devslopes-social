4fd4328b4bd5495a529116fdbec0270443349fdb
Jess Rascal <Jess@Jessey.uk>
2016-07-24 21:53:18 +0100

    Completed auto sign in

---
+++ devslopes-social/FeedVC.swift
@@ -0,0 +1,29 @@
+//
+//  FeedVC.swift
+//  devslopes-social
+//
+//  Created by Jess Rascal on 24/07/2016.
+//  Copyright © 2016 JustOneJess. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import SwiftKeychainWrapper
+
+class FeedVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func singInTapped(_ sender: AnyObject) {
+        let keychainResult = KeychainWrapper.removeObjectForKey(KEY_UID)
+        print("JESS: ID removed from keychain \(keychainResult)")
+        try! FIRAuth.auth()?.signOut()
+        performSegue(withIdentifier: "goToSignIn", sender: nil)
+    }
+    
+
+}
