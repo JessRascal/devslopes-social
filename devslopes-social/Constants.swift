12ddcef830267a71fbe28cf46d0c37174523fc6c
Jess Rascal <Jess@Jessey.uk>
2016-07-24 18:18:31 +0100

    Sign in ui custom classes complete

---
+++ devslopes-social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  devslopes-social
+//
+//  Created by Jess Rascal on 24/07/2016.
+//  Copyright © 2016 JustOneJess. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
